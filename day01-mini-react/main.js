57deac610a8b8b88b0413f5f19d91632a5a4a0a1
stevenaces <3313899200@qq.com>
2024-01-13 19:31:01 +0800

    feat: v2 抽象vdom,硬编码显示"app"

--- day01-mini-react/main.js
+++ day01-mini-react/main.js
@@ -1,10 +1,37 @@
 // console.log("main.js");
 
 // v1
-const dom = document.createElement("div");
-dom.id = "app";
+// const dom = document.createElement("div");
+// dom.id = "app";
+// document.querySelector("#root").append(dom);
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = "app";
+// dom.append(textNode);
+
+// v2
+// react -> vdom -> javascript object
+// vdom: type, props, children
+const textEle = {
+	type: "TEXT_ELEMENT",
+	props: {
+		nodeValue: "app",
+		children: [],
+	},
+};
+
+const el = {
+	type: "div",
+	props: {
+		id: "app",
+		children: [textEle],
+	},
+};
+
+const dom = document.createElement(el.type);
+dom.id = el.props.id;
 document.querySelector("#root").append(dom);
 
 const textNode = document.createTextNode("");
-textNode.nodeValue = "app";
+textNode.nodeValue = textEle.props.nodeValue;
 dom.append(textNode);
