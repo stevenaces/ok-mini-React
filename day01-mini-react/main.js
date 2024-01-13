2b8883a2b306cc56f957a1bfc1288f8425936e9d
stevenaces <3313899200@qq.com>
2024-01-13 19:40:29 +0800

    feat: v3 实现创建vdom对象方法，硬编码显示"app"

--- day01-mini-react/main.js
+++ day01-mini-react/main.js
@@ -12,26 +12,58 @@
 // v2
 // react -> vdom -> javascript object
 // vdom: type, props, children
-const textEle = {
-	type: "TEXT_ELEMENT",
-	props: {
-		nodeValue: "app",
-		children: [],
-	},
-};
+// const textEle = {
+// 	type: "TEXT_ELEMENT",
+// 	props: {
+// 		nodeValue: "app",
+// 		children: [],
+// 	},
+// };
 
-const el = {
-	type: "div",
-	props: {
-		id: "app",
-		children: [textEle],
-	},
-};
+// const el = {
+// 	type: "div",
+// 	props: {
+// 		id: "app",
+// 		children: [textEle],
+// 	},
+// };
 
-const dom = document.createElement(el.type);
-dom.id = el.props.id;
+// const dom = document.createElement(el.type);
+// dom.id = el.props.id;
+// document.querySelector("#root").append(dom);
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEle.props.nodeValue;
+// dom.append(textNode);
+
+// v3 create vdom object function
+function createTextNode(text) {
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			nodeValue: text,
+			children: [],
+		},
+	};
+}
+
+function createElement(type, props, ...children) {
+	return {
+		type,
+		props: {
+			...props,
+			children,
+		},
+	};
+}
+
+const textNode = createTextNode("app");
+const App = createElement("div", { id: "app" }, textNode);
+
+const dom = document.createElement(App.type);
+dom.id = App.props.id;
 document.querySelector("#root").append(dom);
 
-const textNode = document.createTextNode("");
-textNode.nodeValue = textEle.props.nodeValue;
-dom.append(textNode);
+const textNodeEl = document.createTextNode("");
+textNodeEl.nodeValue = textNode.props.nodeValue;
+dom.append(textNodeEl);
