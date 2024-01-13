c371fc41fb6b04a6cda854a9925df53e3ff9ca24
stevenaces <3313899200@qq.com>
2024-01-13 19:27:01 +0800

    feat: v1 硬编码在浏览器上打印"app"

---
+++ day01-mini-react/main.js
@@ -0,0 +1,10 @@
+// console.log("main.js");
+
+// v1
+const dom = document.createElement("div");
+dom.id = "app";
+document.querySelector("#root").append(dom);
+
+const textNode = document.createTextNode("");
+textNode.nodeValue = "app";
+dom.append(textNode);
