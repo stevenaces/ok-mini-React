7a908296664db2f1203acca3bfe555e605cc3959
stevenaces <3313899200@qq.com>
2024-01-13 20:17:03 +0800

    feat: v4.1 优化文本节点vdom创建

--- day01-mini-react/main.js
+++ day01-mini-react/main.js
@@ -1,41 +1,3 @@
-// console.log("main.js");
-
-// v1
-// const dom = document.createElement("div");
-// dom.id = "app";
-// document.querySelector("#root").append(dom);
-
-// const textNode = document.createTextNode("");
-// textNode.nodeValue = "app";
-// dom.append(textNode);
-
-// v2
-// react -> vdom -> javascript object
-// vdom: type, props, children
-// const textEle = {
-// 	type: "TEXT_ELEMENT",
-// 	props: {
-// 		nodeValue: "app",
-// 		children: [],
-// 	},
-// };
-
-// const el = {
-// 	type: "div",
-// 	props: {
-// 		id: "app",
-// 		children: [textEle],
-// 	},
-// };
-
-// const dom = document.createElement(el.type);
-// dom.id = el.props.id;
-// document.querySelector("#root").append(dom);
-
-// const textNode = document.createTextNode("");
-// textNode.nodeValue = textEle.props.nodeValue;
-// dom.append(textNode);
-
 // v3 create vdom object function
 function createTextNode(text) {
 	return {
@@ -52,22 +14,12 @@ function createElement(type, props, ...children) {
 		type,
 		props: {
 			...props,
-			children,
+			children: children.map((child) =>
+				typeof child === "string" ? createTextNode(child) : child
+			),
 		},
 	};
 }
-
-const textNode = createTextNode("app");
-const textNode2 = createTextNode(" mini-react");
-const App = createElement("div", { id: "app" }, textNode, textNode2);
-
-// const dom = document.createElement(App.type);
-// dom.id = App.props.id;
-// document.querySelector("#root").append(dom);
-
-// const textNodeEl = document.createTextNode("");
-// textNodeEl.nodeValue = textNode.props.nodeValue;
-// dom.append(textNodeEl);
 
 // v4 dynamic create vdom and render
 function render(el, container) {
@@ -91,4 +43,5 @@ function render(el, container) {
 	container.append(dom);
 }
 
+const App = createElement("div", { id: "app" }, "Hi ", "mini-react");
 render(App, document.querySelector("#root"));
