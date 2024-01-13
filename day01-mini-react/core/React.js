01d3a96dc7126cfb78a4617644437f9ff355efb3
stevenaces <3313899200@qq.com>
2024-01-13 20:38:55 +0800

    feat: v5 代码拆分，模拟React的API

---
+++ day01-mini-react/core/React.js
@@ -0,0 +1,49 @@
+function createTextNode(text) {
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			nodeValue: text,
+			children: [],
+		},
+	};
+}
+
+function createElement(type, props, ...children) {
+	return {
+		type,
+		props: {
+			...props,
+			children: children.map((child) =>
+				typeof child === "string" ? createTextNode(child) : child
+			),
+		},
+	};
+}
+
+function render(el, container) {
+	const dom =
+		el.type === "TEXT_ELEMENT"
+			? document.createTextNode("")
+			: document.createElement(el.type);
+
+	// 处理 props
+	Object.keys(el.props).forEach((key) => {
+		if (key !== "children") {
+			dom[key] = el.props[key];
+		}
+	});
+
+	// 处理 children
+	el.props.children.forEach((e) => {
+		render(e, dom);
+	});
+
+	container.append(dom);
+}
+
+const React = {
+	render,
+	createElement,
+};
+
+export default React;
