c99e698a8690e7a1df525728ee72457d1d5b6556
stevenaces <3313899200@qq.com>
2024-01-13 21:34:14 +0800

    feat: mini-react 支持 jsx

---
+++ day01-mini-react-jsx/App.jsx
@@ -0,0 +1,17 @@
+import React from "./core/React.js";
+
+// const App = React.createElement("div", { id: "app" }, "Hi ", "mini-react", "!");
+const App = <div>Hi mini-react!</div>;
+
+// function AppOne() {
+//   return <div>Hi mini-react!</div>
+// }
+
+console.log(App);
+
+// 暂时还不支持函数组件
+// function App(){
+//   return <div>Hi React function component</div>
+// }
+
+export default App;
