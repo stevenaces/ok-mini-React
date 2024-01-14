951cd978c3aa8c4d07e9075dbaeb74b644e210e1
stevenaces <3313899200@qq.com>
2024-01-14 12:34:38 +0800

    feat: 构建mini-react-fiber架构

--- day01-mini-react-jsx/core/React.js
+++ day01-mini-react-jsx/core/React.js
@@ -9,7 +9,6 @@ function createTextNode(text) {
 }
 
 function createElement(type, props, ...children) {
-	console.log("creatElement jsx");
 	return {
 		type,
 		props: {
@@ -22,24 +21,90 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-	const dom =
-		el.type === "TEXT_ELEMENT"
-			? document.createTextNode("")
-			: document.createElement(el.type);
+	nextWorkUnit = {
+		dom: container,
+		props: {
+			children: [el],
+		},
+	};
+}
 
-	// 处理 props
-	Object.keys(el.props).forEach((key) => {
+let nextWorkUnit = null;
+function workLoop(deadline) {
+	let shouldYield = false;
+
+	while (!shouldYield && nextWorkUnit) {
+		nextWorkUnit = performWorkUnit(nextWorkUnit);
+		shouldYield = deadline.timeRemaining() < 1;
+	}
+
+	requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+// 创建dom
+function createDom(type) {
+	return type === "TEXT_ELEMENT"
+		? document.createTextNode("")
+		: document.createElement(type);
+}
+
+// 处理props
+function updateProps(dom, props) {
+	Object.keys(props).forEach((key) => {
 		if (key !== "children") {
-			dom[key] = el.props[key];
+			dom[key] = props[key];
 		}
 	});
+}
 
-	// 处理 children
-	el.props.children.forEach((e) => {
-		render(e, dom);
+// 构建fiber链
+function initChildren(fiber) {
+	const children = fiber.props.children;
+	let prevChild = null;
+	children.forEach((child, index) => {
+		const newFiber = {
+			type: child.type,
+			props: child.props,
+			parent: fiber,
+			child: null,
+			sibling: null,
+			dom: null,
+		};
+		if (index === 0) {
+			fiber.child = newFiber;
+		} else {
+			prevChild.sibling = newFiber;
+		}
+		prevChild = newFiber;
 	});
+}
 
-	container.append(dom);
+function performWorkUnit(fiber) {
+	if (!fiber.dom) {
+		// 1. 创建dom
+		const dom = (fiber.dom = createDom(fiber.type));
+
+		fiber.parent.dom.append(dom);
+
+		// 2. 处理props
+		updateProps(dom, fiber.props);
+	}
+
+	// 3. 构建关系
+	initChildren(fiber);
+
+	// 4. 返回下一个任务
+	if (fiber.child) {
+		return fiber.child;
+	}
+
+	if (fiber.sibling) {
+		return fiber.sibling;
+	}
+
+	return fiber.parent?.sibling;
 }
 
 const React = {
